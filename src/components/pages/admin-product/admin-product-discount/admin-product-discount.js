d11fb6c1c49886a69acbd09fc9fc36ba3ecdad53
Phi Duc <phivanduc325@gmail.com>
2025-07-12 06:35:32 +0700

    Fix Giam gia chung

--- src/components/pages/admin-product/admin-product-discount/admin-product-discount.js
+++ src/components/pages/admin-product/admin-product-discount/admin-product-discount.js
@@ -25,170 +25,194 @@ import { getProductTypes } from "@/lib/api/server-action/product-type";
 import { getGeneralDiscounts } from "@/lib/api/server-action/general-discount";
 
 export default function AdminProductDiscount() {
-    const form = useForm({
-        defaultValues: {
-            discounts: []
-        }
-    });
+    // const form = useForm({
+    //     defaultValues: {
+    //         discounts: []
+    //     }
+    // });
 
-    const formArray = useFieldArray({
-        control: form.control,
-        name: "discounts"
-    });
+    // const formArray = useFieldArray({
+    //     control: form.control,
+    //     name: "discounts"
+    // });
 
-    const [products, setProducts] = useState([]);
-    const [submitting, setSubmitting] = useState(false);
-    const [discountSelected, setDiscountSelected] = useState(null);
+    // const [products, setProducts] = useState([]);
+    // const [submitting, setSubmitting] = useState(false);
+    // const [discountSelected, setDiscountSelected] = useState(null);
     
     const [productTypes, setProductTypes] = useState([]);
     const [categories, setCategories] = useState([]);
-    const [livingSpaces, setLivingSpaces] = useState([]);
+    // const [livingSpaces, setLivingSpaces] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState();
 
     useEffect(() => {
         (async () => {
-            const [generalDiscountsRes, productTypesRes, categoriesRes, livingSpacesRes] = await Promise.all([
-                getGeneralDiscounts(),
-                getProductTypes(),
-                getCategories(),
-                getLivingSpaces()
-            ]);
+            // const [generalDiscountsRes, productTypesRes, categoriesRes, livingSpacesRes] = await Promise.all([
+            //     getGeneralDiscounts(),
+            //     getProductTypes(),
+            //     getCategories(),
+            //     getLivingSpaces()
+            // ]);
 
-            const { status: generalDiscountsStatus, result: generalDiscounts } = generalDiscountsRes;
-            const { status: productTypesStatus, result: productTypes } = productTypesRes;
-            const { status: categoriesStatus, result: categories } = categoriesRes;
-            const { status: livingSpacesStatus, result: livingSpaces } = livingSpacesRes;
+            // const { status: generalDiscountsStatus, result: generalDiscounts } = generalDiscountsRes;
+            // const { status: productTypesStatus, result: productTypes } = productTypesRes;
+            // const { status: categoriesStatus, result: categories } = categoriesRes;
+            // const { status: livingSpacesStatus, result: livingSpaces } = livingSpacesRes;
 
-            if (!generalDiscounts?.success) {
-                setError(`${generalDiscountsStatus},${generalDiscounts?.message} - Giảm giá chung`);
-                setLoading(false);
-                return;
-            }
+            // if (!generalDiscounts?.success) {
+            //     setError(`${generalDiscountsStatus},${generalDiscounts?.message} - Giảm giá chung`);
+            //     setLoading(false);
+            //     return;
+            // }
+
+            // if (!productTypes?.success) {
+            //     setError(`${productTypesStatus},${productTypes?.message} - Loại sản phẩm`);
+            //     setLoading(false);
+            //     return;
+            // }
+
+            // if (!categories?.success) {
+            //     setError(`${categoriesStatus},${categories?.message} - Danh mục`);
+            //     setLoading(false);
+            //     return;
+            // }
+
+            // if (!livingSpaces?.success) {
+            //     setError(`${livingSpacesStatus},${livingSpaces?.message} - Không gian sống`);
+            //     setLoading(false);
+            //     return;
+            // }
+
+            // form.reset({
+            //     discounts: generalDiscounts?.data?.general_discounts?.map(item => ({ ...item, discountAmount: `${convertToNumberDb(item?.discountAmount)}` })) || []
+            // });
+
+            // setProductTypes(productTypes?.data?.product_types);
+            // setCategories(categories?.data?.categories);
+            // setLivingSpaces(livingSpaces?.data?.living_spaces);
+
+            // setLoading(false);
+            const { status, result: productTypes } = await getProductTypes();
+
+            console.log(productTypes);
 
             if (!productTypes?.success) {
-                setError(`${productTypesStatus},${productTypes?.message} - Loại sản phẩm`);
+                setError(`${status},${productTypes?.message} - Loại sản phẩm`);
                 setLoading(false);
                 return;
             }
-
-            if (!categories?.success) {
-                setError(`${categoriesStatus},${categories?.message} - Danh mục`);
-                setLoading(false);
-                return;
-            }
-
-            if (!livingSpaces?.success) {
-                setError(`${livingSpacesStatus},${livingSpaces?.message} - Không gian sống`);
-                setLoading(false);
-                return;
-            }
-
-            form.reset({
-                discounts: generalDiscounts?.data?.general_discounts?.map(item => ({ ...item, discountAmount: `${convertToNumberDb(item?.discountAmount)}` })) || []
-            });
 
             setProductTypes(productTypes?.data?.product_types);
-            setCategories(categories?.data?.categories);
-            setLivingSpaces(livingSpaces?.data?.living_spaces);
-
             setLoading(false);
         })();
     }, []);
     
-    const currentDiscount = form.watch(`discounts.${discountSelected}`);
+    // const currentDiscount = form.watch(`discounts.${discountSelected}`);
 
-    useEffect(() => {
-        if (formArray.fields.length && discountSelected === null) {
-            setDiscountSelected(0);
-        }
-    }, [formArray.fields.length]);
+    // useEffect(() => {
+    //     if (formArray.fields.length && discountSelected === null) {
+    //         setDiscountSelected(0);
+    //     }
+    // }, [formArray.fields.length]);
 
-    useEffect(() => {
-        setProducts([]);
-    }, [currentDiscount]);
+    // useEffect(() => {
+    //     setProducts([]);
+    // }, [currentDiscount]);
 
-    const onSubmit = async (data) => {
-        if (submitting) return;
-        setSubmitting(true);
+    // const onSubmit = async (data) => {
+    //     if (submitting) return;
+    //     setSubmitting(true);
 
-        const generalDiscount = currentDiscount?.rootId ?
-        await editGeneralDiscount({ ...data?.discounts[discountSelected], discountAmount: convertToNumber(data?.discounts[discountSelected]?.discountAmount) }, currentDiscount?.rootId) :
-        await addGeneralDiscount(data?.discounts[discountSelected]);
+    //     const generalDiscount = currentDiscount?.rootId ?
+    //     await editGeneralDiscount({ ...data?.discounts[discountSelected], discountAmount: convertToNumber(data?.discounts[discountSelected]?.discountAmount) }, currentDiscount?.rootId) :
+    //     await addGeneralDiscount(data?.discounts[discountSelected]);
 
-        console.log(data?.discounts[discountSelected]);
+    //     console.log(data?.discounts[discountSelected]);
 
-        const message = generalDiscount?.message;
-        if (generalDiscount?.success) {
-            toast.success(message);
+    //     const message = generalDiscount?.message;
+    //     if (generalDiscount?.success) {
+    //         toast.success(message);
 
-            if (!currentDiscount?.rootId) {
-                formArray.update(discountSelected, {
-                    ...formArray?.fields[discountSelected],
-                    rootId: generalDiscount?.data?.general_discount?.id
-                });
-            }
-        }
-        else toast.error(message);
+    //         if (!currentDiscount?.rootId) {
+    //             formArray.update(discountSelected, {
+    //                 ...formArray?.fields[discountSelected],
+    //                 rootId: generalDiscount?.data?.general_discount?.id
+    //             });
+    //         }
+    //     }
+    //     else toast.error(message);
 
-        setSubmitting(false);
-    }
+    //     setSubmitting(false);
+    // }
 
     if (loading) return <MainLoading />
     if (error) return <Error message={error} />
 
     return (
-        <div className="space-y-[30px]">
-            <header className="space-y-[5px]">
-                <h1 className="text-[24px] font-semibold">Giảm giá chung</h1>
-                <p className="text-[14px] text-darkMedium font-medium">Bạn có thể tạo ra nhiều giảm giá áp dụng cho nhiều sản phẩm khác nhau dựa vào bộ lọc.</p>
-            </header>
-
-            <Form {...form}>
-                <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="space-y-[20px]"
-                >
-                    <AdminProductDiscountTab
-                        formArray={formArray}
-                        discountSelected={discountSelected}
-                        setDiscountSelected={setDiscountSelected}
-                    />
-
-                    {
-                        typeof discountSelected === "number" &&
-                        ( 
-                            <AdminProductDiscountForm
-                                key={discountSelected}
-                                form={form}
-                                formArray={formArray}
-                                index={discountSelected}
-                                data={{
-                                    productTypes,
-                                    categories,
-                                    livingSpaces
-                                }}
-                                setDiscountSelected={setDiscountSelected}
-                                submitting={submitting}
-                            />
+        <div className="space-y-[10px]">
+            <p className="text-[16px] font-medium">Fetch loại sản phẩm.</p>
+            <ul className="space-y-[5px]">
+                {
+                    productTypes.map(prodType => {
+                        return (
+                            <li key={prodType.id}>{prodType.slug}</li>
                         )
-                    }
-                </form>
-            </Form>
-
-            {
-                currentDiscount &&
-                <TooltipProvider>
-                    <AdminProductDiscountTable
-                        form={form}
-                        formArray={formArray}
-                        index={discountSelected}
-                        currentDiscount={currentDiscount}
-                        products={products}
-                        setProducts={setProducts}
-                    />
-                </TooltipProvider>
-            }
+                    })
+                }
+            </ul>
         </div>
+        // <div className="space-y-[30px]">
+        //     <header className="space-y-[5px]">
+        //         <h1 className="text-[24px] font-semibold">Giảm giá chung</h1>
+        //         <p className="text-[14px] text-darkMedium font-medium">Bạn có thể tạo ra nhiều giảm giá áp dụng cho nhiều sản phẩm khác nhau dựa vào bộ lọc.</p>
+        //     </header>
+
+        //     <Form {...form}>
+        //         <form
+        //             onSubmit={form.handleSubmit(onSubmit)}
+        //             className="space-y-[20px]"
+        //         >
+        //             <AdminProductDiscountTab
+        //                 formArray={formArray}
+        //                 discountSelected={discountSelected}
+        //                 setDiscountSelected={setDiscountSelected}
+        //             />
+
+        //             {
+        //                 typeof discountSelected === "number" &&
+        //                 ( 
+        //                     <AdminProductDiscountForm
+        //                         key={discountSelected}
+        //                         form={form}
+        //                         formArray={formArray}
+        //                         index={discountSelected}
+        //                         data={{
+        //                             productTypes,
+        //                             categories,
+        //                             livingSpaces
+        //                         }}
+        //                         setDiscountSelected={setDiscountSelected}
+        //                         submitting={submitting}
+        //                     />
+        //                 )
+        //             }
+        //         </form>
+        //     </Form>
+
+        //     {
+        //         currentDiscount &&
+        //         <TooltipProvider>
+        //             <AdminProductDiscountTable
+        //                 form={form}
+        //                 formArray={formArray}
+        //                 index={discountSelected}
+        //                 currentDiscount={currentDiscount}
+        //                 products={products}
+        //                 setProducts={setProducts}
+        //             />
+        //         </TooltipProvider>
+        //     }
+        // </div>
     )
 }
